96aebec54a2fc284242e2477fddf299fdd0bdde8
Sabare vijay <sabarevijay2001@gmail.com>
2025-03-14 16:36:00 +0530

    Attendance completed-2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import {Provider} from 'react-redux'
 import { persistor, store } from './redux/Store.js'
 import { PersistGate } from 'redux-persist/integration/react'
-import store from './redux/Store.js';
+// import store from './redux/Store.js';
 
 
 createRoot(document.getElementById('root')).render(
