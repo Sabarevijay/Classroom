e8f839ac1bd2ba6d6f844264bef8744aab4efd8d
Sabare vijay <sabarevijay2001@gmail.com>
2025-03-15 10:20:59 +0530

    Attendance completed-18

--- server/models/user.js
+++ server/models/user.js
@@ -16,7 +16,7 @@ const UserSchema=new mongoose.Schema({
     role:{
         type:String,
         enum:['admin','user'],
-        default:'admin'
+        default:'user'
     }
 },{timestamps:true})
 
