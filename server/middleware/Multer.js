3317baa0376ae2917a2089af445471612762dd35
Sabare vijay <sabarevijay2001@gmail.com>
2025-03-15 09:56:15 +0530

    Attendance completed-13

--- server/middleware/Multer.js
+++ server/middleware/Multer.js
@@ -6,7 +6,8 @@ const storage =multer.diskStorage({
         cb(null,'public/images')
     },
     filename:function(req,file,cb){
-        cb(null,Date.now()+'-'+path.extname(file.originalname))
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null,uniqueSuffix +path.extname(file.originalname))
     }
 })
 const upload= multer({storage:storage})
