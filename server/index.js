e9bcd27761cebc74149c8e993c4cca4b943173c9
Sabare vijay <sabarevijay2001@gmail.com>
2025-04-23 14:18:03 +0530

    index

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+    res.setHeader("Cross-Origin-Opener-Policy", "unsafe-none");
     next();
 });
 
